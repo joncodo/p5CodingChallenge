34184470b6fa92e3331f7aef7deed9ce7bde0546
Jonathan O'Donnell <jodonnell@broadsoft.com>
2016-12-25 15:30:24 -0400

    Update main.js

--- day2/main.js
+++ day2/main.js
@@ -1,10 +1,12 @@
+//This code does not really work as intended since the p5js library does not support it yet.
+
+//https://github.com/processing/p5.js/issues/1720
+
 var a = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   translate(width / 2, height / 2);
-
-
 }
 
 function draw() {
