cc2e7ac72f97b496ac267a38fa2ab01fbb695f5e
joncodo <jonathan.carl.odonnell@gmail.com>
2016-12-25 18:08:50 -0400

    tail grows

--- day3/snake.js
+++ day3/snake.js
@@ -5,8 +5,7 @@ function Snake() {
   this.y = 0;
   this.xSpeed = 1;
   this.ySpeed = 0;
-  this.total = 1;
-  this.tail = [];
+  this.tail = [ {x:0,y:0} ];
 
   this.update = function() {
     this.x += this.xSpeed * speed;
@@ -14,22 +13,35 @@ function Snake() {
 
     this.x = constrain(this.x, 0, width - speed);
     this.y = constrain(this.y, 0, height - speed);
+
+    //push new pos to front of array
+    this.tail.unshift({x: this.x, y: this.y});
+    //remove last array element
+    this.tail.pop();
   };
 
   this.show = function() {
     fill(255);
     noStroke();
-    rect(this.x, this.y, speed, speed);
+
+    //Draw Snake
+    for (var i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, speed, speed);
+    }
   };
 
   this.dir = function(x,y) {
+    //set direction based on keypress
     this.xSpeed = x;
     this.ySpeed = y;
   };
 
   this.eat = function(food) {
+    //Did you collide with the food?
     var d = dist(this.x, this.y, food.x, food.y);
     if (d < 1) {
+      //Add to the snake tail when you eat food
+      this.tail.unshift({x: this.x, y: this.y});
       this.total += 1;
       return true;
     } else {
